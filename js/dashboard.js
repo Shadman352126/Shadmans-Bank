cd231dff687bae7bf31b73ba1118e33852940d7c
Shadman352126 <shadman352126@gmail.com>
2023-08-19 02:40:29 +0600

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,53 +1,3 @@
-// deposit amount box
-// document.getElementById("btn-deposit").addEventListener("click", function () {
-//   const depositAmountField = document.getElementById("deposit-input");
-//   const newDepositAmountString = depositAmountField.value;
-//   const newDepositAmount = parseFloat(newDepositAmountString);
-
-//   const depositTotalElement = document.getElementById("deposit-total");
-//   const previousDepositTotalString = depositTotalElement.innerText;
-//   const previousDepositTotal = parseFloat(previousDepositTotalString);
-//   const currentTotalDeposit = previousDepositTotal + newDepositAmount;
-
-//   depositTotalElement.innerText = currentTotalDeposit;
-//   depositAmountField.value = "";
-//   // deposit balance add to total balance
-//   const balanceTotalElement = document.getElementById("balance-total");
-//   const previousBalanceTotalString = balanceTotalElement.innerText;
-//   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-//   const currentTotalBalance = previousBalanceTotal + newDepositAmount;
-//   balanceTotalElement.innerText = currentTotalBalance;
-// });
-
-// withdraw amount box
-// document.getElementById("btn-withdraw").addEventListener("click", function () {
-//   const withdrawAmountField = document.getElementById("withdraw-input");
-//   const newWithdrawAmountString = withdrawAmountField.value;
-//   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-
-//   const withdrawTotalElement = document.getElementById("withdraw-total");
-//   const previousWithdrawTotalString = withdrawTotalElement.innerText;
-//   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-//   const currentTotalWithdraw = previousWithdrawTotal + newWithdrawAmount;
-
-//   // Withdraw balance substract from total balance
-//   const balanceTotalElement = document.getElementById("balance-total");
-//   const previousBalanceTotalString = balanceTotalElement.innerText;
-//   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-
-//   withdrawAmountField.value = "";
-
-//   if (newWithdrawAmount > previousBalanceTotal) {
-//     alert("tui fokir");
-//     return;
-//   }
-
-//   withdrawTotalElement.innerText = currentTotalWithdraw;
-
-//   const currentTotalBalance = previousBalanceTotal - newWithdrawAmount;
-//   balanceTotalElement.innerText = currentTotalBalance;
-// });
-
 function depositCalculation() {
   const depositAmountField = document.getElementById("deposit-input");
   const newDepositAmountString = depositAmountField.value;
@@ -105,3 +55,53 @@ function withdrawCalculation() {
   const currentTotalBalance = previousBalanceTotal - newWithdrawAmount;
   balanceTotalElement.innerText = currentTotalBalance;
 }
+
+// deposit amount box
+// document.getElementById("btn-deposit").addEventListener("click", function () {
+//   const depositAmountField = document.getElementById("deposit-input");
+//   const newDepositAmountString = depositAmountField.value;
+//   const newDepositAmount = parseFloat(newDepositAmountString);
+
+//   const depositTotalElement = document.getElementById("deposit-total");
+//   const previousDepositTotalString = depositTotalElement.innerText;
+//   const previousDepositTotal = parseFloat(previousDepositTotalString);
+//   const currentTotalDeposit = previousDepositTotal + newDepositAmount;
+
+//   depositTotalElement.innerText = currentTotalDeposit;
+//   depositAmountField.value = "";
+//   // deposit balance add to total balance
+//   const balanceTotalElement = document.getElementById("balance-total");
+//   const previousBalanceTotalString = balanceTotalElement.innerText;
+//   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+//   const currentTotalBalance = previousBalanceTotal + newDepositAmount;
+//   balanceTotalElement.innerText = currentTotalBalance;
+// });
+
+// withdraw amount box
+// document.getElementById("btn-withdraw").addEventListener("click", function () {
+//   const withdrawAmountField = document.getElementById("withdraw-input");
+//   const newWithdrawAmountString = withdrawAmountField.value;
+//   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+//   const withdrawTotalElement = document.getElementById("withdraw-total");
+//   const previousWithdrawTotalString = withdrawTotalElement.innerText;
+//   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+//   const currentTotalWithdraw = previousWithdrawTotal + newWithdrawAmount;
+
+//   // Withdraw balance substract from total balance
+//   const balanceTotalElement = document.getElementById("balance-total");
+//   const previousBalanceTotalString = balanceTotalElement.innerText;
+//   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+//   withdrawAmountField.value = "";
+
+//   if (newWithdrawAmount > previousBalanceTotal) {
+//     alert("tui fokir");
+//     return;
+//   }
+
+//   withdrawTotalElement.innerText = currentTotalWithdraw;
+
+//   const currentTotalBalance = previousBalanceTotal - newWithdrawAmount;
+//   balanceTotalElement.innerText = currentTotalBalance;
+// });
